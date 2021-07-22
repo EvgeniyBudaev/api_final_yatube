c66cc68058a37b43f5cc5be6f66324a3c97349ef
Evgeniy Budaev <budaev.e@gmail.com>
2021-07-22 11:10:03 +0300

    fix

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -102,6 +102,5 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1), 'AUTH_HEADER_TYPES': ('Bearer',),
 }
